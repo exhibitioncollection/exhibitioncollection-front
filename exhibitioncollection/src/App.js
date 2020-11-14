fb72090674542ad2ce3ed6492873c1abf5a0913a
jungwook <neity16@daum.net>
2020-11-14 16:31:06 +0900

    라우팅 추가

--- exhibitioncollection/src/App.js
+++ exhibitioncollection/src/App.js
@@ -1,7 +1,18 @@
+import {
+  BrowserRouter as Router, Route, Switch
+} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Detail from './components/Detail/Detail';
+
 function App() {
   return (
-    <>
-    </>
+    <Router>
+        <Switch>
+          <Route exact path='/' component={Home}></Route>
+          <Route exact path='/:id' component={Detail}></Route>
+          <Route path='/*'>404 NOT FOUND</Route>
+        </Switch>
+    </Router>
   );
 }
 
